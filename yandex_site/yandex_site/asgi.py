692b79b0770568e66d47fdf3a9044d7e545d615f
Alex <askiselev00@gmail.com>
2021-01-30 16:02:29 +0300

    init

--- yandex_site/yandex_site/asgi.py
+++ yandex_site/yandex_site/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for yandex_parser project.
+ASGI config for yandex_site project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yandex_parser.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yandex_site.settings')
 
 application = get_asgi_application()
